88531a8eff922a9ecc74c2784621e79b33fa6df4
Can KAYA <cankaya07@gmail.com>
2018-06-12 14:35:46 +0300

    rblist

--- quotesbot/spiders/Reebok/ProductListPage.py
+++ quotesbot/spiders/Reebok/ProductListPage.py
@@ -5,15 +5,14 @@ import json
 
 class RBProductListPage(scrapy.Spider):
     name = "ReebokProductListPage"
-   
     start_urls = [
-        "https://www.reebok.com.tr/erkek-spor-ayakkabi?ps=5000"
+        'https://www.reebok.com.tr/erkek-spor-ayakkabi?ps=5000',
     ]
 
     def parse(self, response):
         for quote in response.xpath('//*[@id="department-body"]/div/div[2]/div[3]/div'):
             yield {
                 'ProductName': quote.xpath('./div/div[5]/div[2]/a/text()').extract_first(),
-                'ProductUrl': self.start_urls[0]+quote.xpath('./div/div[5]/div[2]/a/@href').extract_first(),
-                'OnClick': self.start_urls[0]+quote.xpath('./div/div[5]/div[2]/a/@onlick').extract_first(),
+                'ProductUrl': quote.xpath('./div/div[5]/div[2]/a/@href').extract_first(),
+                'Additional': quote.xpath('./div/div[5]/div[2]/a/@onclick').extract_first(),
             }
